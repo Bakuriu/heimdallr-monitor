34a68b40238ea9c131860f195453c94e189b3f41
Giacomo Alzetta <giacomo.alzetta@gmail.com>
2018-10-30 07:51:58 +0100

    dev: updated version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='heimdallr',
-    version='0.1.0',
+    version='0.2.0',
     author='Giacomo Alzetta',
     author_email='giacomo.alzetta+heimdallr@gmail.com',
     scripts=['heimdallr'],
